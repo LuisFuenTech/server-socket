7bcb3b9e7e8318eb4f7a08ad072ad3d43a5a9bfa
luisfuentech <luisfuentes.au@gmail.com>
2020-11-08 12:06:43 -0500

    Perf: Some changes

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 
 //Settings
-app.set('port', process.env.PORT || 3000);
+app.set("port", process.env.PORT || 3000);
 
 //Static files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-const server = app.listen(app.get('port'), () => {
-    console.log(`Server\'s working on port ${app.get('port')}`);
+const server = app.listen(app.get("port"), () => {
+  console.log(`Server's working on port ${app.get("port")}`);
 });
 
 //Websockets
-const socketIO = require('socket.io');
+const socketIO = require("socket.io");
 const io = socketIO(server);
 
-io.on('connection', (socket) => {
-    console.log(`New connection: ${socket.id}`);
+io.on("connection", socket => {
+  console.log(`New connection: ${socket.id}`);
 
-    //Recibe datos y reenvía
-    socket.on('chat:msg', (data) => {
-        io.sockets.emit('server:msg', data);
-    });
+  //Recibe datos y reenvía
+  socket.on("chat:msg", data => {
+    io.sockets.emit("server:msg", data);
+  });
 
-    socket.on('chat:typing', (data) => {
-        socket.broadcast.emit('server:type', data);
-    });
+  socket.on("chat:typing", data => {
+    socket.broadcast.emit("server:type", data);
+  });
 });
-
